72f76c0aefa64aa83461c32089e7bd8ab74ce4a4
unknown <lewisokware@gmail.com>
2022-08-16 14:22:00 +0300

    links 2

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -6,13 +6,13 @@ const GetStarted = () => (
     <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
       <div className={`${styles.flexStart} flex-row`}>
         <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
-          <a href="https://www.jonmaurice.org/"><span className="text-gradient">Get</span></a>
+          <a href="https://www.jonmaurice.org/"><span className="text-gradient">Open</span></a>
         </p>
         <a href="https://www.jonmaurice.org/"><img src={arrowUp} alt="arrow-up" className="w-[23px] h-[23px] object-contain" /></a>
       </div>
       
       <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
-        <a href="https://www.jonmaurice.org/"><span className="text-gradient">Started</span></a>
+        <a href="https://www.jonmaurice.org/"><span className="text-gradient">App</span></a>
       </p>
     </div>
   </div>
